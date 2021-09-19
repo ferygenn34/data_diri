525f5d2f86a80a92e97d2e332d02faf7778d9730
ferygenn34 <sfery7898@gmail.com>
2021-09-19 13:50:23 +0700

    Add files via upload

---
+++ biodataa.php
@@ -0,0 +1,43 @@
+<?php
+$nama = "fery setyawan";
+$nim = "192520043";
+$alamat = "pogungjurutengah, bayan, Purworejo";
+$email = "sfery7898@gmail.com";
+$minat = "pemrograman PHP";
+?>
+
+<body bgcolor="#ADD8E6">
+<body>
+<center><h1><font color="blue" size="6"> Data Diri</h1></center></font>
+<center><table cellspacing="15" cellpadding="8">
+</body>
+<table>
+<img src="ferygenn.JPEG" width="200" height="200">
+<tr>
+<td rowspan="9">
+<td><?php echo "Nama"; ?></td>
+<td><?php echo ":"; ?></td>
+<td><?php echo $nama ?></td>
+</td>
+</tr>
+<tr>
+<td><?php echo "NIM"; ?></td>
+<td><?php echo ":"; ?></td>
+<td><?php echo $nim ;?></td>
+</tr>
+<tr>
+<td><?php echo "Alamat"; ?></td>
+<td><?php echo ":"; ?></td>
+<td><?php echo $alamat ;?></td>
+</tr>
+<tr>
+<td><?php echo "Email"; ?></td>
+<td><?php echo ":"; ?></td>
+<td><?php echo $email ;?></td>
+</tr>
+<tr>
+<td><?php echo "Minat"; ?></td>
+<td><?php echo ":"; ?></td>
+<td><?php echo $minat ;?></td>
+</tr>
+</table>
